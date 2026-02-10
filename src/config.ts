bb99e3d7658defbbcbff9d8835702418a288d607
Juicy233 <136211653+Juicy-awa@users.noreply.github.com>
2026-02-10 13:53:20 +0800

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -9,7 +9,7 @@ import { LinkPreset } from "./types/config";
 
 export const siteConfig: SiteConfig = {
 	title: "Fuwari",
-	subtitle: "Demo Site",
+	subtitle: "Blog",
 	lang: "zh_CN", // 'en', 'zh_CN', 'zh_TW', 'ja', 'ko', 'es', 'th'
 	themeColor: {
 		hue: 250, // Default hue for the theme color, from 0 to 360. e.g. red: 0, teal: 200, cyan: 250, pink: 345
@@ -53,14 +53,14 @@ export const navBarConfig: NavBarConfig = {
 };
 
 export const profileConfig: ProfileConfig = {
-	avatar: "assets/images/demo-avatar.png", // Relative to the /src directory. Relative to the /public directory if it starts with '/'
+	avatar: "assets/images/image.png", // Relative to the /src directory. Relative to the /public directory if it starts with '/'
 	name: "Mi Manchi.",
-	bio: "万语难尽涩于口 祈尔繁芜胜长春",
+	bio: "𝐽𝑢𝑠𝑡 𝐿𝑖𝑙𝑖𝑡ℎ.",
 	links: [
 		{
 			name: "Wechat",
 			icon: "tabler:brand-wechat",
-			url: "https://cloudpaste-backend.juicy233.dpdns.org/api/file-view/AmOciS",
+			url: "https://cloudpaste.528ab038514f3b3461ae62fd0393caf4.r2.cloudflarestorage.com/IMG_1344.jpeg?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Content-Sha256=UNSIGNED-PAYLOAD&X-Amz-Credential=6fff91ed3040f6f8400a9ade4a751f15%2F20260210%2Fauto%2Fs3%2Faws4_request&X-Amz-Date=20260210T053050Z&X-Amz-Expires=3600&X-Amz-Signature=0c3a32540ccb6e57d8b4ae35e331a8b8254c06271938dc47d468227d389f3e80&X-Amz-SignedHeaders=host&response-content-disposition=inline%3B%20filename%3D%22IMG_1344.jpeg%22&response-content-type=image%2Fjpeg&x-id=GetObject",
 		},
 		{
 			name: "Bilibili",
